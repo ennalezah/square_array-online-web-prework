630a888d3505fb76cda9367af2007777fccaf044
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-08 23:49:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 numbers = [2, 4, 6, 8]
 
 def square_array(numbers)
-   
-  sq_numbers = []
+   sq_numbers = []
   
   numbers.each do |num|
-    sq_numbers << (num **2)
+    sq_numbers << num**2
   end
 end
