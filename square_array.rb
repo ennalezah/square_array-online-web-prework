7aafeadeee890d845abd688594b2aee726f1b280
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-08 23:31:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,6 @@ def square_array(numbers)
   numbers = [2, 4, 6, 8] 
   
   numbers.each do |i|
-    sq_num = (i**2)
-    sq_array = [sq_num]
-    puts "Current number is: #{sq_num}"
+    sq_numbers << (i**2)
   end
 end
